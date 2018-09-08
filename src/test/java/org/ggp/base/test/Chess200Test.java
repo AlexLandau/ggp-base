78dc3a955024ee159d730310d173bc8364ae9623
AlexLandau <alandau@cs.stanford.edu>
2018-09-08 16:29:32 -0700

    Fixes to the Chess200Test

--- src/test/java/org/ggp/base/test/Chess200Test.java
+++ src/test/java/org/ggp/base/test/Chess200Test.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 
 import org.ggp.base.util.game.Game;
 import org.ggp.base.util.game.GameRepository;
+import org.ggp.base.util.game.SimpleLocalGameRepository;
 import org.ggp.base.util.statemachine.MachineState;
 import org.ggp.base.util.statemachine.Move;
 import org.ggp.base.util.statemachine.StateMachine;
@@ -18,27 +19,24 @@ import org.junit.Test;
 public class Chess200Test {
     @Test
     public void test() throws TransitionDefinitionException, MoveDefinitionException {
-        GameRepository repo = GameRepository.getDefaultRepository();
+        GameRepository repo = SimpleLocalGameRepository.getLocalBaseRepo();
+
         Game game = repo.getGame("chess_200");
         ProverStateMachine sm = ProverStateMachineFactory.createNormal().buildInitializedForGame(game);
 
         MachineState state = sm.getInitialState();
-        try {
-            state = checkMovesAreLegalThenApply(sm, state, "(move pawn e 2 e 4)", "noop");
-            state = checkMovesAreLegalThenApply(sm, state, "noop", "(move pawn e 7 e 6)");
-            state = checkMovesAreLegalThenApply(sm, state, "(move pawn e 4 e 5)", "noop");
-            state = checkMovesAreLegalThenApply(sm, state, "noop", "(move knight b 8 a 6)");
-            state = checkMovesAreLegalThenApply(sm, state, "(move king e 1 e 2)", "noop");
-            state = checkMovesAreLegalThenApply(sm, state, "noop", "(move knight a 6 b 8)");
-            state = checkMovesAreLegalThenApply(sm, state, "(move king e 2 d 3)", "noop");
-            state = checkMovesAreLegalThenApply(sm, state, "noop", "(move knight b 8 a 6)");
-            state = checkMovesAreLegalThenApply(sm, state, "(move king d 3 c 4)", "noop");
-            state = checkMovesAreLegalThenApply(sm, state, "noop", "(move pawn d 7 d 5)");
-            // En passant captures should work when in check
-            state = checkMovesAreLegalThenApply(sm, state, "(move pawn e 5 d 6)", "noop");
-        } catch (RuntimeException e) {
-            throw new RuntimeException("Moves legal in state were: " + sm.getLegalMovesByRole(state), e);
-        }
+        state = checkMovesAreLegalThenApply(sm, state, "(move pawn e 2 e 4)", "noop");
+        state = checkMovesAreLegalThenApply(sm, state, "noop", "(move pawn e 7 e 6)");
+        state = checkMovesAreLegalThenApply(sm, state, "(move pawn e 4 e 5)", "noop");
+        state = checkMovesAreLegalThenApply(sm, state, "noop", "(move knight b 8 a 6)");
+        state = checkMovesAreLegalThenApply(sm, state, "(move king e 1 e 2)", "noop");
+        state = checkMovesAreLegalThenApply(sm, state, "noop", "(move knight a 6 b 8)");
+        state = checkMovesAreLegalThenApply(sm, state, "(move king e 2 d 3)", "noop");
+        state = checkMovesAreLegalThenApply(sm, state, "noop", "(move knight b 8 a 6)");
+        state = checkMovesAreLegalThenApply(sm, state, "(move king d 3 c 4)", "noop");
+        state = checkMovesAreLegalThenApply(sm, state, "noop", "(move pawn d 7 d 5)");
+        // En passant captures should work when in check
+        state = checkMovesAreLegalThenApply(sm, state, "(move pawn e 5 d 6)", "noop");
     }
 
     private MachineState checkMovesAreLegalThenApply(StateMachine sm, MachineState state, String... moveNames) throws MoveDefinitionException, TransitionDefinitionException {
